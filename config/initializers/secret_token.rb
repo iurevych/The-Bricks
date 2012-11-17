2eea65705ffa9f9975b45c12e94f405e40518487
iurevych <iurevych@gmail.com>
2012-11-17 15:53:12 +0200

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBricks::Application.config.secret_token = '6b6926549ec5bca3180a4d3abca53f5a6a6006390e2f5f2260e49bcffb86fccb667bf596a978b702cd274c1f9e724dfbdc4dbdebb9d042d50a32ccf4b3945ef7'
